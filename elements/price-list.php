b0c2d8a8416c6d0d6dfb0233a7359091609143f5
anutka03 <akotelnikova@diferenteweb.com>
2023-10-17 14:09:03 +0300

    Блоки: прайс-лист, форма: остались вопросы

--- elements/price-list.php
+++ elements/price-list.php
@@ -1,47 +1,58 @@
-<div class="price-list group">
-    <div class="col-9">
-        <p class="text-102">Цены на основной ряд услуг</p>
-        <div class="rectangle-2323-holder">
-            Название услуги
-        </div>
-        <p class="text-104">Замена масла в двигателе</p>
-        <div class="rectangle-2325-holder">
-            Компьютерная диагностика
-        </div>
-        <p class="text-106">Диагностика подвески</p>
-        <div class="rectangle-2326-holder">
-            Техническое обслуживание
-        </div>
-        <p class="text-108">Замена масла в двигателе</p>
-        <div class="rectangle-2327-holder">
-            Компьютерная диагностика
-        </div>
-        <p class="text-110">Диагностика подвески</p>
-        <div class="rectangle-2328-holder">
-            Техническое обслуживание
-        </div>
-        <div class="group-42833 group">
-            <img class="vector-10" src="images/vector_10.png" alt="" width="28" height="34" />
-            <a href="" class="text-112">Скачать прайс-лист</a>
+<div class="container price-list">
+    <div class="row">
+        <div class="col-md-12">
+            <p class="text-102">Цены на основной ряд услуг</p>
         </div>
     </div>
-    <div class="col-10 group">
-        <div class="rectangle-2324"></div>
-        <p class="text-113">от 600 Р</p>
-        <div class="rectangle-2325-holder-2">
-            от 1200 Р
+    <div class="row mt-5">
+        <div class="col-md-9">
+            <div class="rectangle-2323-holder">
+                Название услуги
+            </div>
+            <p class="text-104">Замена масла в двигателе</p>
+            <div class="rectangle-2326-holder">
+                Компьютерная диагностика
+            </div>
+            <p class="text-106">Диагностика подвески</p>
+            <div class="rectangle-2326-holder">
+                Техническое обслуживание
+            </div>
+            <p class="text-108">Замена масла в двигателе</p>
+            <div class="rectangle-2326-holder">
+                Компьютерная диагностика
+            </div>
+            <p class="text-110">Диагностика подвески</p>
+            <div class="rectangle-2326-holder">
+                Техническое обслуживание
+            </div>
         </div>
-        <p class="text-115">от 899 Р</p>
-        <div class="rectangle-2326-holder-2">
-            от 2500 Р
+        <div class="col-md-3">
+            <div class="rectangle-2324"></div>
+            <p class="text-113">от 600 Р</p>
+            <div class="rectangle-2325-holder-2">
+                от 1200 Р
+            </div>
+            <p class="text-115">от 899 Р</p>
+            <div class="rectangle-2326-holder-2">
+                от 2500 Р
+            </div>
+            <p class="text-117">от 600 Р</p>
+            <div class="rectangle-2329-holder">
+                от 1200 Р
+            </div>
+            <p class="text-119">от 899 Р</p>
+            <div class="rectangle-2330-holder">
+                от 2500 Р
+            </div>
         </div>
-        <p class="text-117">от 600 Р</p>
-        <div class="rectangle-2329-holder">
-            от 1200 Р
-        </div>
-        <p class="text-119">от 899 Р</p>
-        <div class="rectangle-2330-holder">
-            от 2500 Р
+    </div>
+    <div class="row mt-3">
+        <div class="col-md-12">
+            <div class="d-flex justify-content-center">
+                <img class="vector-10" src="images/vector_10.png" alt="" width="28" height="34" />
+                <a href="" class="text-112">Скачать прайс-лист</a>
+            </div>
         </div>
     </div>
 </div>
+
