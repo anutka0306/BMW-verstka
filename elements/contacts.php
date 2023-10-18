98d65b95b7b7386f4e96b97bb22190e039f0cd8e
anutka03 <akotelnikova@diferenteweb.com>
2023-10-18 15:10:34 +0300

    Блоки: Контакты и Футер

--- elements/contacts.php
+++ elements/contacts.php
@@ -1,91 +1,109 @@
 <div class="contacts group">
-    <p class="kontakty-2">Контакты</p>
-    <div class="row-18 group">
-        <div class="fc-icons">
-            <div class="rectangle-2321-holder">
-                <div class="place_ddvmcmqawwo1-3">
-                    <div class="group-8">
-                        <div class="vector-11"></div>
-                        <img class="vector-12" src="images/vector_18.png" alt="" width="13" height="18" />
-                        <img class="vector-13" src="images/vector_19.png" alt="" width="13" height="6" />
-                    </div>
-                </div>
-            </div>
-            <div class="rectangle-2320-holder">
-                <div class="place_ddvmcmqawwo1-2">
-                    <div class="group-9">
-                        <div class="vector-14"></div>
-                        <img class="vector-15" src="images/vector_15.png" alt="" width="13" height="19" />
-                        <img class="vector-16" src="images/vector_16.png" alt="" width="13" height="6" />
-                    </div>
-                </div>
-            </div>
-            <div class="rectangle-2319-holder">
-                <div class="place_ddvmcmqawwo1-1">
-                    <div class="group-10">
-                        <div class="vector-17"></div>
-                        <img class="vector-18" src="images/vector_12.png" alt="" width="13" height="19" />
-                        <img class="vector-19" src="images/vector_13.png" alt="" width="13" height="6" />
-                    </div>
-                </div>
-            </div>
-            <div class="rectangle-2318-holder">
-                <div class="mail_a9gikujpkvyx-1">
-                    <img class="vector-20" src="images/vector_21.png" alt="" width="19" height="10" />
-                </div>
-            </div>
-            <div class="rectangle-2322-holder">
-                <div class="mail_a9gikujpkvyx-2">
-                    <div class="calendar_ty9r71iu4at0-1">
-                        <img class="vector-21" src="images/vector_25.png" alt="" width="20" height="6" />
-                        <div class="row-14 group">
-                            <div class="col-51">
-                                <div class="row-32 group">
-                                    <div class="vector-22"></div>
-                                    <div class="vector-23"></div>
-                                </div>
-                                <div class="row-33 group">
-                                    <div class="vector-24"></div>
-                                    <div class="vector-25"></div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-5">
+                <p class="kontakty-2">Контакты</p>
+                <div class="mt-5">
+                    <div class="contacts__list_item">
+                        <div class="contacts__list_icon">
+                            <div class="place_ddvmcmqawwo1-3">
+                                <div class="group-8">
+                                    <div class="vector-11"></div>
+                                    <img class="vector-12" src="images/vector_18.png" alt="" width="13" height="18" />
+                                    <img class="vector-13" src="images/vector_19.png" alt="" width="13" height="6" />
                                 </div>
                             </div>
-                            <div class="col-50 group">
-                                <div class="row-30 group">
-                                    <div class="vector-26"></div>
-                                    <div class="vector-27"></div>
-                                </div>
-                                <div class="row-31 group">
-                                    <div class="vector-28"></div>
-                                    <div class="vector-29"></div>
+                        </div>
+                        <p class="text-130">
+                            <strong class="fw700 text-white">Кушелевская 20</strong><br />
+                            8 (812) 913-05-38
+                        </p>
+                    </div>
+                    <div class="contacts__list_item">
+                        <div class="contacts__list_icon">
+                            <div class="place_ddvmcmqawwo1-3">
+                                <div class="group-8">
+                                    <div class="vector-11"></div>
+                                    <img class="vector-12" src="images/vector_18.png" alt="" width="13" height="18" />
+                                    <img class="vector-13" src="images/vector_19.png" alt="" width="13" height="6" />
                                 </div>
                             </div>
+                        </div>
+                        <p class="text-131">
+                            <strong class="fw700 text-white">Боровая 116</strong><br />
+                            8 (812) 999-85-53
+                        </p>
+                    </div>
+                    <div class="contacts__list_item">
+                        <div class="contacts__list_icon">
+                            <div class="place_ddvmcmqawwo1-3">
+                                <div class="group-8">
+                                    <div class="vector-11"></div>
+                                    <img class="vector-12" src="images/vector_18.png" alt="" width="13" height="18" />
+                                    <img class="vector-13" src="images/vector_19.png" alt="" width="13" height="6" />
+                                </div>
+                            </div>
+                        </div>
+                        <p class="text-132">
+                            Дорога на Турухтанные Острова 12<br />
+                            <span class="fw400 text-white">8 (812) 000-00-00</span>
+                        </p>
+                    </div>
+                    <div class="contacts__list_item">
+                        <div class="contacts__list_icon">
+                            <div class="mail_a9gikujpkvyx-1">
+                                <img class="vector-20" src="images/vector_21.png" alt="" width="19" height="10" />
+                            </div>
+                        </div>
+                        <p class="text-133">
+                            <strong class="text-style-4 text-white">E-mail</strong><br />
+                            vrn@acpik.ru
+                        </p>
+                    </div>
+                    <div class="contacts__list_item">
+                        <div class="contacts__list_icon">
+                            <div class="mail_a9gikujpkvyx-2">
+                                <div class="calendar_ty9r71iu4at0-1">
+                                    <img class="vector-21" src="images/vector_25.png" alt="" width="20" height="6" />
+                                    <div class="row-14 group">
+                                        <div class="col-51">
+                                            <div class="row-32 group">
+                                                <div class="vector-22"></div>
+                                                <div class="vector-23"></div>
+                                            </div>
+                                            <div class="row-33 group">
+                                                <div class="vector-24"></div>
+                                                <div class="vector-25"></div>
+                                            </div>
+                                        </div>
+                                        <div class="col-50 group">
+                                            <div class="row-30 group">
+                                                <div class="vector-26"></div>
+                                                <div class="vector-27"></div>
+                                            </div>
+                                            <div class="row-31 group">
+                                                <div class="vector-28"></div>
+                                                <div class="vector-29"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="text-134">Режим работы</p>
+                            <p class="text-135">
+                                Ежедневно<br />
+                                с 09:00 до 21:00
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-31">
-            <p class="text-130">
-                <strong class="fw700">Кушелевская 20</strong><br />
-                8 (812) 913-05-38
-            </p>
-            <p class="text-131">
-                <strong class="fw700">Боровая 116</strong><br />
-                8 (812) 999-85-53
-            </p>
-            <p class="text-132">
-                Дорога на Турухтанные Острова 12<br />
-                <span class="fw400">8 (812) 000-00-00</span>
-            </p>
-            <p class="text-133">
-                <strong class="text-style-4">E-mail</strong><br />
-                vrn@acpik.ru
-            </p>
-            <p class="text-134">Режим работы</p>
-            <p class="text-135">
-                Ежедневно<br />
-                с 09:00 до 21:00
-            </p>
+
+            <div class="col-md-7">
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A6fea9ea9fcda30864cca8832dc4ba94a28396573ab7cb1956d717f9e90697df6&amp;width=100%25&amp;height=556&amp;lang=ru_RU&amp;scroll=true"></script>
+            </div>
         </div>
     </div>
 </div>
--- elements/footer.php
+++ elements/footer.php
@@ -1,4 +1,4 @@
-<div class="footer">
+<!--<div class="footer">
     <div class="footer-end">
         <div class="l-constrained-11 group">
             <div class="col-34">
@@ -40,6 +40,87 @@
             <div class="rectangle-2297"></div>
             <img class="image-531" src="images/image_531.png" alt="" width="36" height="29" />
             <img class="image-532" src="images/image_532.png" alt="" width="36" height="29" />
+        </div>
+    </div>
+</div>-->
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="footer__logo_col">
+                    <div class="d-flex">
+                        <div class="rectangle-2174-holder">
+                            Автоцентр
+                        </div>
+                        <p class="pik-2">ПИК</p>
+                    </div>
+                    <div class="">
+                        <div class="row-43 group">
+                            <p class="specializirovannyi-2">Специализированный</p>
+                        </div>
+                        <p class="text-136">автосервис <span class="color1c69d4">BMW</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="container footer__menu">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="footer__menu_first_col">
+                                <p>Меню</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <ul>
+                                <li>
+                                    <a href="#">Главная</a>
+                                </li>
+                                <li>
+                                    <a href="#">Услуги</a>
+                                </li>
+                                <li>
+                                    <a href="#">Наши работы</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <ul>
+                                <li>
+                                    <a href="#">Запчасти</a>
+                                </li>
+                                <li>
+                                    <a href="#">Тюнинг</a>
+                                </li>
+                                <li>
+                                    <a href="#">Контакты</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="container footer__social">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="footer__social_first_col">
+                                <p>Мы<br>в соцсетях</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="footer__social_icons">
+                                <a href="#" class="me-2"><img src="/images/footer-vk.svg" alt="vk"></a>
+                                <a href="#"><img src="/images/footer-youtube.svg" alt="youtube"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p class="footer__copyright">© 2023 / Все права защищены<br>Не является публичной офертой</p>
+            </div>
         </div>
     </div>
 </div>
