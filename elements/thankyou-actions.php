6e502099b629e39e12a2e07bb00770073f2693d2
anutka03 <akotelnikova@diferenteweb.com>
2023-10-29 18:06:03 +0200

    thankyou Страница

---
+++ elements/thankyou-actions.php
@@ -0,0 +1,66 @@
+<div class="container repairing-and-improving">
+    <p class="title-dex">Дальнейшие действия</p>
+
+    <div class="chess-repairing-and-improving">
+        <!-- First line -->
+        <div class="chess-repairing-and-improving_row">
+            <div class="chess-repairing-and-improving_row_col chess-repairing-and-improving_row_col_40 pe-2">
+                <div class="chess-repairing-and-improving_row_col__img thankyou-actions_row_col__img1">
+                    <div class="col-20_wrapper justify-content-between">
+                        <h3>Консультация</h3>
+                        <p class="text-61"><img src="images/01.png" width="120" alt=""></p>
+                    </div>
+
+                    <div class="d-flex thankyou-actions_row_col_text1">
+                        <div class="rectangle-2259"></div>
+                        <p class="text-62">Наши мастера - эксперты  по всем вопросам, связанным с ремонтом  и эксплуатацией автомобиля.  Они помогут разобраться  Вам с автомобильной терминологией, говоря  на простом и понятном языке.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="chess-repairing-and-improving_row_col chess-repairing-and-improving_row_col_60 pe-2">
+                <div class="chess-repairing-and-improving_row_col__img thankyou-actions_row_col__img2">
+                    <div class="col-20_wrapper justify-content-between">
+                        <h3>Подбор деталей</h3>
+                        <p class="text-61"><img src="images/02.png" alt=""></p>
+                    </div>
+
+                    <div class="d-flex thankyou-actions_row_col_text2">
+                        <div class="rectangle-2259"></div>
+                        <p class="text-62">Что выбрать – оригинал или аналог? Если аналог, то какой?  А где заказать дешевле и чтобы быстрее доставили?  На эти и другие вопросы по запчастям наши менеджеры знают ответы.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Second line -->
+        <div class="chess-repairing-and-improving_row">
+            <div class="chess-repairing-and-improving_row_col chess-repairing-and-improving_row_col_60 pe-2">
+                <div class="chess-repairing-and-improving_row_col__img thankyou-actions_row_col__img3">
+                    <div class="col-20_wrapper justify-content-between">
+                        <h3>Ремонт и техническое <br/>обслуживание</h3>
+                        <p class="text-61"><img src="images/03.png" alt=""></p>
+                    </div>
+
+                    <div class="d-flex thankyou-actions_row_col_text3">
+                        <div class="rectangle-2259"></div>
+                        <p class="text-62">Наши специалисты могут провести полное и своевременное техническое обслуживание BMW. По разным признакам они могут выявить причины неисправностей, предупредить  и устранить поломки, а также быстро  и точно осуществить диагностику  и отремонтировать Ваш автомобиль.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="chess-repairing-and-improving_row_col chess-repairing-and-improving_row_col_40 pe-2">
+                <div class="chess-repairing-and-improving_row_col__img thankyou-actions_row_col__img4">
+                    <div class="col-20_wrapper justify-content-between">
+                        <h3>Гарантия <br/>качества</h3>
+                        <p class="text-61"><img src="images/04.png" alt=""></p>
+                    </div>
+
+                    <div class="d-flex thankyou-actions_row_col_text4">
+                        <div class="rectangle-2259"></div>
+                        <p class="text-62">Внимательно относитесь  к выбору автоцентра,  которому доверяете обслуживание и ремонт  своего автомобиля.  Мы предоставляем только качественным сервис и смело даем гарантию до 24 месяцев.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
---
+++ thank-you.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0, minimal-ui">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="robots" content="noindex" />
+        <title>BMW</title>
+
+        <!-- Fonts -->
+        <link rel="preload" href="css/fonts/montserrat/Montserrat-SemiBold.woff" as="font" type="font/woff" crossorigin />
+        <link rel="preload" href="css/fonts/montserrat/Montserrat-Bold.woff" as="font" type="font/woff" crossorigin />
+        <link rel="preload" href="css/fonts/montserrat/Montserrat-Medium.woff" as="font" type="font/woff" crossorigin />
+        <link rel="preload" href="css/fonts/montserrat/Montserrat-Black.woff" as="font" type="font/woff" crossorigin />
+        <link rel="preload" href="css/fonts/montserrat/Montserrat-Regular.woff" as="font" type="font/woff" crossorigin />
+        <link rel="preload" href="css/fonts/montserrat/Montserrat-Light.woff" as="font" type="font/woff" crossorigin />
+        <link rel="preload" href="./css/fonts/dinpro.otf" as="font" type="font/opentype" crossorigin />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic&amp;subset=cyrillic,cyrillic-ext,latin,latin-ext,vietnamese">
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&amp;display=swap" />
+        <link rel="stylesheet" href="css/fonts/cambria/stylesheet.css">
+
+        <!-- Bootsrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
+
+        <!-- Slick & Swiper -->
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+        <link rel="stylesheet" href="css/slick-theme.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/css/swiper.min.css">
+
+        
+        <link rel="shortcut icon" type="image/png" href="./favicon.png" />
+        <meta name="og:type" content="website" />
+        <link type="text/css" rel="stylesheet" href="css/theme.css?v=1.<? echo time()?>" />
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.min.js"></script>
+    </head>
+<body dark-scroll>
+<div class="lp">
+    <?php include 'elements/top-nav.php';?>
+    <?php include 'elements/header-top.php';?>
+    <div class="container">
+        <h1 class="thankyou__header">
+            Спасибо за заказ
+        </h1>
+        <div class="line-after-heading mt-3 mb-3" style="width: 55px;"></div>
+        <p class="thankyou__subtitle">
+            Автоцентр ПИК - постгарантийное обслуживание автомобилей BMW в Санкт-Петербурге. <br>Наша сила в людях.
+        </p>
+        <div class="list-ul group">
+            <div class="d-flex align-items-center mb-3">
+                <div class="maker">
+                    <img class="arrow-1" src="images/arrow_1_1.png" alt="arrow" />
+                </div>
+                <p class="text-71">100% команды составляют механики с профильным опытом от 5 лет, 50% - от 10 лет.</p>
+            </div>
+
+            <div class="d-flex align-items-center mb-3">
+                <div class="maker">
+                    <img class="arrow-1" src="images/arrow_1_1.png" alt="arrow" />
+                </div>
+                <p class="text-71">Делаем все – от простой замены масла до капитального ремонта двигателя.</p>
+            </div>
+
+            <div class="d-flex align-items-center mb-3">
+                <div class="maker">
+                    <img class="arrow-1" src="images/arrow_1_1.png" alt="arrow" />
+                </div>
+                <p class="text-71">Мы работаем напрямую с производителем, поэтому стоимость оригинальных запчастей у нас на 40% дешевле, чем у дилера.</p>
+            </div>
+
+            <div class="d-flex align-items-center mb-3">
+                <div class="maker">
+                    <img class="arrow-1" src="images/arrow_1_1.png" alt="arrow" />
+                </div>
+                <p class="text-71">Склад более 100.000 позиций: в наличии всегда есть прокладки, шкивы, вкладыши и пр. комплектующие.</p>
+            </div>
+
+        </div>
+        <p class="thankyou__subtitle">
+            Автоцентр ПИК - это гарантия качества и минимальные сроки за доступные деньги!
+        </p>
+    </div>
+
+    <!-- Gallery -->
+    <div class="container short-autocenter">
+        <div class="row short-autocenter-gallery">
+            <div class="row col-md-12 pb-4">
+                <div class="col-md-5"><img src="images/thankyou/gal-1.png" alt="" height="240" /></div>
+                <div class="col-md-3"><img src="images/thankyou/gal-2.png" alt="" height="240" /></div>
+                <div class="col-md-4"><img src="images/thankyou/gal-3.png" alt="" height="240" /></div>
+            </div>
+            <div class="row col-md-12">
+                <div class="col-md-4"><img src="images/thankyou/gal-4.png" alt="" height="240" /></div>
+                <div class="col-md-3"><img src="images/thankyou/gal-5.png" alt="" height="240" /></div>
+                <div class="col-md-5"><img src="images/thankyou/gal-6.png" alt="" height="240" /></div>
+            </div>
+        </div>
+    </div>
+
+    <?php include 'elements/thankyou-actions.php';?>
+
+    <?php include 'elements/l-unconstraind-2.php';?>
+    <?php include 'elements/cliente-reviews.php';?>
+
+    <?php include 'elements/footer.php';?>
+</div>
+
+<script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="js/swiper-bundle.min.js"></script>
+<script>
+var video_swiper = new Swiper('.swiper.slider-video-review', {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 37,
+    loop: true,
+    pagination: {
+      el: ".swiper-video-review-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      420: {
+        slidesPerView: 2
+      },
+      767: {
+        slidesPerView: 3
+      },     
+      992: {
+        slidesPerView: 4
+      },       
+      1240: {
+        slidesPerView: 4
+      },
+      1530: {
+        slidesPerView: 4
+      },
+    }  
+});
+var check_swiper = new Swiper('.swiper.check-slider', {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-check-slider-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      420: {
+        slidesPerView: 2
+      },
+      767: {
+        slidesPerView: 3
+      },     
+      992: {
+        slidesPerView: 3
+      },       
+      1240: {
+        slidesPerView: 3
+      },
+      1530: {
+        slidesPerView: 3
+      },
+    }  
+});
+
+
+
+$('.quiz-slider').slick({
+	dots: true,
+	speed: 300,
+	slidesToShow: 4,
+	rows:3,
+	slidesToScroll:1,
+	arrows:false,
+	focusOnSelect:false,
+	focusOnChange:false,
+	centerMode:false,
+	infinite: false,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: false,
+				focusOnSelect:false,
+				focusOnChange:false,
+				dots: true
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				focusOnSelect:false,
+				focusOnChange:false,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 2,
+				focusOnSelect:false,
+				focusOnChange:false,
+				centerMode:false,
+				infinite: false,
+				slidesToScroll: 2
+			}
+		}
+	]
+});
+$('.quiz-intro-slider').slick({
+	dots: false,
+	speed: 300,
+	slidesToShow: 3,
+	//slide:'.quiz-thumbnail',
+	rows:1,
+	variableWidth: true,
+	slidesToScroll:1,
+	arrows:false,
+	responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        arrows:true,
+	        dots: false
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        arrows:true,
+	        dots: false
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        arrows:true,
+	        dots: false
+	      }
+	    }
+	]
+});
+
+$('.managers-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:true,
+                dots: true
+            }
+        },
+    ]
+});
+
+$('.cliente-reviews-slick1').slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:true,
+                dots: true
+            }
+        },
+    ]
+}).next();
+
+
+$('.cliente-reviews-slick2').slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:true,
+                dots: true
+            }
+        },
+    ]
+}).next();
+
+$('.cliente-reviews-slick3').slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:true,
+                dots: true
+            }
+        },
+    ]
+}).next();
+
+$('.cliente-reviews-slick4').slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:true,
+                dots: true
+            }
+        },
+    ]
+}).next();
+
+$('.cliente-reviews-slick5').slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:true,
+                dots: true
+            }
+        },
+    ]
+}).next();
+
+</script>
+</body>
+</html>
