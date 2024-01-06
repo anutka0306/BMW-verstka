11ab80ce1640a8cb8b4930862afc431ee3cc6d2d
anutka03 <akotelnikova@diferenteweb.com>
2024-01-06 21:51:19 +0200

    новый дизайн - меню в шапке

--- elements/header-top.php
+++ elements/header-top.php
@@ -1,28 +1,35 @@
 <div class="header-top">
-    <div class="container" style="display: flex;">
-        <div class="block-logo">
-            <div class="logo">
-                <a href="/">
-                    <div class="bg-holder">Автоцентр</div>
-                    <div class="pik">ПИК</div>
-                </a>
+    <div class="container">
+        <div class="row">
+            <div class="block-logo col-md-4">
+                <div class="logo">
+                    <a href="/">
+                        <div class="bg-holder">Автоцентр</div>
+                        <div class="pik">ПИК</div>
+                    </a>
+                </div>
+                <div class="service-description">
+                    Специализированный автосервис <span class="color1c69d4">BMW</span>
+                </div>
             </div>
-            <div class="service-description">
-                Специализированный автосервис <span class="color1c69d4">BMW</span>
+            <div class="certificates__header col-md-2">
+                <img src="/images/certificate.png" alt="Сертификат">
+                <img src="/images/certificate.png" alt="Сертификат">
+                <img src="/images/certificate.png" alt="Сертификат">
             </div>
-        </div>
-        <div class="address-heading d-none d-md-flex">
-            <div class="b116">
-                <img class="subtract" src="images/subtract_3.png" alt="" width="14" height="22" />
-                <p class="text-3">ул. Боровая 116<br />+7(812) 955 85-53</p>
-            </div>
-            <div class="k20">
-                <img class="subtract-2" src="images/subtract_2.png" alt="" width="15" height="22" />
-                <p class="text-4">Кушелевская дорога 20<br />+7(812) 936 33-79</p>
-            </div>
-            <div class="to12">
-                <img class="subtract-3" src="images/subtract.png" alt="" width="15" height="22" />
-                <p class="text-5">Дор. на Турухтанные Острова 12<br />+7(812) 000 00-00</p>
+            <div class="address-heading d-none d-md-flex col-md-6">
+                <div class="b116">
+                    <p class="text-3">ул. Боровая 116<br /><strong>+7(812) 955 85-53</strong></p>
+                    <small>Работаем сейчас</small>
+                </div>
+                <div class="k20">
+                    <p class="text-4">Кушелевская дорога 20<br /><strong>+7(812) 936 33-79</strong></p>
+                    <small>Работаем сейчас</small>
+                </div>
+                <div class="to12">
+                    <p class="text-5">Дор. на Турухтанные Острова 12<br /><strong>+7(812) 000 00-00</strong></p>
+                    <small>График работы с 9:00- 21:00</small>
+                </div>
             </div>
         </div>
     </div>
