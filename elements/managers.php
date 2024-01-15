6c108efb3afd9934887ad4eea9fd6134d4660f93
anutka03 <akotelnikova@diferenteweb.com>
2024-01-15 19:55:01 +0200

    новый дизайн - managers блок

--- elements/managers.php
+++ elements/managers.php
@@ -1,41 +1,53 @@
 <div class="managers">
+    <div class="container">
     <p class="title-7">Сотрудники отвечающие за отдел BMW</p>
-    <div class="wrapper-5 managers-slider">
+    <div class="managers-slider">
         <div class="manager-slider-item">
-            <img src="images/manager1.png" alt="manager 1">
-            <div class="manager-slider-title">
-                <p class="text-73">Шайхутдинов Ильдар</p>
-                <p class="text-74">Мастер-приёмщик</p>
+            <div class="manager-slider-item__wrapper">
+                <img src="images/manager1.png" alt="manager 1">
+                <div class="manager-slider-title">
+                    <p class="text-73">Шайхутдинов <br>Ильдар</p>
+                    <p class="text-74">Мастер-приёмщик</p>
+                </div>
             </div>
         </div>
         <div class="manager-slider-item">
-            <img src="images/manager2.png" alt="manager 2">
-            <div class="manager-slider-title">
-                <p class="text-73">Садовой Василий</p>
-                <p class="text-74">Менеджер по подбору запчастей</p>
+            <div class="manager-slider-item__wrapper">
+                <img src="images/manager2.png" alt="manager 2">
+                <div class="manager-slider-title">
+                    <p class="text-73">Садовой <br>Василий</p>
+                    <p class="text-74">Менеджер по подбору запчастей</p>
+                </div>
             </div>
         </div>
         <div class="manager-slider-item">
-            <img src="images/manager3.png" alt="manager 3">
-            <div class="manager-slider-title">
-                <p class="text-73">Антошкин Николай</p>
-                <p class="text-74">Мастер-приёмщик</p>
+            <div class="manager-slider-item__wrapper">
+                <img src="images/manager3.png" alt="manager 3">
+                <div class="manager-slider-title">
+                    <p class="text-73">Антошкин <br>Николай</p>
+                    <p class="text-74">Мастер-приёмщик</p>
+                </div>
             </div>
         </div>
         <div class="manager-slider-item">
-            <img src="images/manager4.png" alt="manager 4">
-            <div class="manager-slider-title">
-                <p class="text-73">Кащук Сергей</p>
-                <p class="text-74">Руководитель сервиса</p>
+            <div class="manager-slider-item__wrapper">
+                <img src="images/manager4.png" alt="manager 4">
+                <div class="manager-slider-title">
+                    <p class="text-73">Кащук <br>Сергей</p>
+                    <p class="text-74">Руководитель сервиса</p>
+                </div>
             </div>
         </div>
 
         <div class="manager-slider-item">
-            <img src="images/manager1.png" alt="manager 1">
-            <div class="manager-slider-title">
-                <p class="text-73">Шайхутдинов Ильдар</p>
-                <p class="text-74">Мастер-приёмщик</p>
+            <div class="manager-slider-item__wrapper">
+                <img src="images/manager1.png" alt="manager 1">
+                <div class="manager-slider-title">
+                    <p class="text-73">Шайхутдинов <br>Ильдар</p>
+                    <p class="text-74">Мастер-приёмщик</p>
+                </div>
             </div>
         </div>
+    </div>
     </div>
 </div>
