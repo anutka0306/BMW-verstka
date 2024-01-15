f6930d459b439f083a1a54dc7616431064a8d16e
anutka03 <akotelnikova@diferenteweb.com>
2024-01-15 13:47:50 +0200

    новый дизайн - choose-service блок

--- elements/choose-service.php
+++ elements/choose-service.php
@@ -1,75 +1,80 @@
 <div class="choose-service">
     <div class="l-constrained-8">
         <p class="choose-service-title">Выберите вид обслуживания BMW</p>
-        <div class="row-15 group">
-            <div class="col-22">
-                <div class="item-cst-3">
-                    <img class="icon-oil" src="images/service-icon1.png" alt="" width="61" />
-                    <p class="link-text-3">
-                        Замена<br />
-                        масла
-                    </p>
-                </div>
-                <div class="item-cst-4">
-                    <img class="icon-convergence" src="images/service-icon7.png" alt="" width="56"  />
-                    <p class="link-text-3">Развал-схождение</p>
-                </div>
-            </div>
-            <div class="col-23">
-                <div class="item-cst-3">
-                    <img class="icon-diagnistc" src="images/service-icon2.png" alt="" width="52"  />
-                    <p class="link-text-3">Диагностика БМВ</p>
-                </div>
-                <div class="item-cst-4">
-                    <img class="icon-car-wheel" src="images/service-icon8.png" alt="" width="56"  />
-                    <p class="link-text-3">
-                        Шиномонтаж<br />
-                        колес
-                    </p>
-                </div>
-            </div>
-            <div class="col-24">
-                <div class="item-cst-3">
-                    <div class="icon-car-service">
-                        <img class="vector" src="images/service-icon3.png" alt="" width="56" />
+        <p class="choose-service-description">Наша команда подробно показывает отчеты о ремонте автомобилей BMW:</p>
+        <div class="container">
+            <div class="row">
+                <div class="choose-service__rows">
+                    <div class="col-22">
+                        <div class="item-cst-3 item-cst">
+                            <img class="icon-oil" src="images/service-icon1.png" alt="" />
+                            <a href="#" class="link-text-3">
+                                Замена<br />
+                                масла
+                            </a>
+                        </div>
+                        <div class="item-cst-4 item-cst">
+                            <img class="icon-convergence" src="images/service-icon7.png" alt=""   />
+                            <a href="#" class="link-text-3">Развал-<br />схождение</a>
+                        </div>
                     </div>
-                    <p class="link-text-3">Тех обслуживание</p>
+                    <div class="col-23">
+                        <div class="item-cst-3 item-cst">
+                            <img class="icon-diagnistc" src="images/service-icon2.png" alt=""  />
+                            <a href="#" class="link-text-3">Диагностика <br />БМВ</a>
+                        </div>
+                        <div class="item-cst-4 item-cst">
+                            <img class="icon-car-wheel" src="images/service-icon8.png" alt=""   />
+                            <a href="#" class="link-text-3">
+                                Шиномонтаж<br />
+                                колес
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-24">
+                        <div class="item-cst-3 item-cst">
+                            <div class="icon-car-service">
+                                <img class="vector" src="images/service-icon3.png" alt=""  />
+                            </div>
+                            <a href="#" class="link-text-3">Тех <br />обслуживание</a>
+                        </div>
+                        <div class="item-cst-4 item-cst">
+                            <img class="vector" src="images/service-icon9.png" alt=""  />
+                            <a href="#" class="link-text-3">Заправка <br />кондея</a>
+                        </div>
+                    </div>
+                    <div class="col-25">
+                        <div class="item-cst-3 item-cst">
+                            <img class="vector" src="images/service-icon4.png" alt=""  />
+                            <a href="#" class="link-text-3">Замена <br />колодок</a>
+                        </div>
+                        <div class="item-cst-4 item-cst">
+                            <img class="vector" src="images/service-icon10.png" alt=""  />
+                            <a href="#" class="link-text-3">Ремонт <br />подвески</a>
+                        </div>
+                    </div>
+                    <div class="col-26">
+                        <div class="item-cst-3 item-cst">
+                            <img class="vector" src="images/service-icon5.png" alt=""  />
+                            <a href="#" class="link-text-3">Замена <br />ГРМ</a>
+                        </div>
+                        <div class="item-cst-4 item-cst">
+                            <img class="vector" src="images/service-icon11.png" alt=""  />
+                            <a href="#" class="link-text-3">Замена <br />свечей</a>
+                        </div>
+                    </div>
+                    <div class="col-27">
+                        <div class="item-cst-11 item-cst">
+                            <img class="vector" src="images/service-icon6.png" alt=""  />
+                            <a href="#" class="link-text-3">Ремонт <br />двигателя</a>
+                        </div>
+                        <div class="item-cst-12 item-cst">
+                            <img class="vector" src="images/service-icon12.png" alt=""  />
+                            <a href="#" class="link-text-3">Ремонт <br />АКПП</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="item-cst-4">
-                    <img class="vector" src="images/service-icon9.png" alt="" width="56" />
-                    <p class="link-text-3">Заправка кондея</p>
-                </div>
-            </div>
-            <div class="col-25">
-                <div class="item-cst-3">
-                    <img class="vector" src="images/service-icon4.png" alt="" width="56" />
-                    <p class="link-text-3">Замена колодок</p>
-                </div>
-                <div class="item-cst-4">
-                    <img class="vector" src="images/service-icon10.png" alt="" width="56" />
-                    <p class="link-text-3">Ремонт подвески</p>
-                </div>
-            </div>
-            <div class="col-26">
-                <div class="item-cst-3">
-                    <img class="vector" src="images/service-icon5.png" alt="" width="56" />
-                    <p class="link-text-3">Замена ГРМ</p>
-                </div>
-                <div class="item-cst-4">
-                    <img class="vector" src="images/service-icon11.png" alt="" width="56" />
-                    <p class="link-text-3">Замена свечей</p>
-                </div>
-            </div>
-            <div class="col-27">
-                <div class="item-cst-11">
-                    <img class="vector" src="images/service-icon6.png" alt="" width="56" />
-                    <p class="link-text-3">Ремонт двигателя</p>
-                </div>
-                <div class="item-cst-12">
-                    <img class="vector" src="images/service-icon12.png" alt="" width="56" />
-                    <p class="link-text-3">Ремонт АКПП</p>
-                </div>
-            </div>
+        </div>
         </div>
     </div>
 </div>
