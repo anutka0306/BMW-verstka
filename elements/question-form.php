120fabf51095cab5d54df91a2ad0c5f28ac8c21f
anutka03 <akotelnikova@diferenteweb.com>
2024-01-18 13:13:33 +0200

    новый дизайн - question-block блок

--- elements/question-form.php
+++ elements/question-form.php
@@ -1,15 +1,16 @@
 <div class="question-form__wrapper">
     <div class="quation-form container">
-    <div class="row">
+        <div class="quation-form__bg"></div>
+    <div class="row quation-form__row">
         <div class="col-12-3">
             <p class="text-121">Остались вопросы?</p>
             <div class="wrapper-16">
                 <div class="col-6-3">
-                    <p class="aleksandr-2">Александр</p>
+                    <p class="aleksandr-2">Ильдар</p>
                     <p class="text-122">Специалист ПИК</p>
                 </div>
-                <p class="text-123">Получите БЕСПЛАТНУЮ консультацию специалиста прямо сейчас</p>
-                <img class="pngegg-17-2" src="images/pngegg_17_2.png" alt="" width="178" height="223" />
+                <p class="text-123">Получите <span>БЕСПЛАТНУЮ</span> консультацию специалиста прямо сейчас</p>
+                <img class="pngegg-17-2" src="images/contact-form-master.png" alt="" />
             </div>
         </div>
         <div class="col-md-6 col-push-6">
@@ -17,23 +18,23 @@
                 <p class="text-124">Свяжитеcь с нами</p>
                 <p class="text-125">
                     Оставьте заявку или звонок<br />
-                    8 (812) 913-05-38
+                    <span>8 (812) 913-05-38</span>
                 </p>
-                <p class="text-126 mt-2">Введите телефон</p>
-                <input type="tel" name="phone" class="form-control">
-                <p class="text-127 mt-2">Выберите адрес</p>
+                <form action="">
+                <input type="tel" name="phone" class="form-control" placeholder="Введите телефон">
                 <select name="address" id="address" class="form-control">
+                    <option value="" selected>Выберите адрес</option>
                     <option value="address1"> Address 1</option>
                     <option value="address2"> Address 2</option>
                 </select>
-                <div class="d-flex mt-2">
+                <div class="d-flex align-items-center mt-2">
                     <input type="checkbox" value="1" class="form-check-input">
                     <p class="text-128 ms-2">
-                        Я даю согласие на обработку<br />
-                        персональных данных
+                        Я даю согласие на обработку персональных данных
                     </p>
                 </div>
-                <input type="submit" class="btn btn-outline-light question-form__form_btn" value="ОТПРАВИТЬ ЗАЯВКУ">
+                <input type="submit" class="btn btn-primary question-form__form_btn" value="ОТПРАВИТЬ ЗАЯВКУ">
+                </form>
             </div>
         </div>
     </div>
