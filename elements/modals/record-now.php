b1ff11755576c56b85f3a2ff4147f56a2e7be976
anutka03 <akotelnikova@diferenteweb.com>
2024-02-05 02:32:44 +0200

    Настройка формы в модальном окне

--- elements/modals/record-now.php
+++ elements/modals/record-now.php
@@ -7,7 +7,7 @@
             </div>
             <div class="modal-body">
                 <div class="container-fluid bd-example-row">
-                    <form method="post" action="#">
+                    <form id="popup_form">
                         <div class="mb-3">
                             <label for="phone" class="form-label">Введите телефон</label>
                             <input required type="tel" name="phone" class="form-control" id="phone">
@@ -16,17 +16,17 @@
                             <label for="address" class="form-label">Выберите адрес</label>
                             <select required class="form-select" name="address" aria-label="Выберите адрес">
                                 <option selected>Выберите адрес</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
+                                <option value="b116">ул. Боровая 116</option>
+                                <option value="k20">Кушелевская дорога 20</option>
+                                <option value="to12">Дор. на Турухтанные Острова 12</option>
                             </select>
                         </div>
                         <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="agree">
+                            <input type="checkbox" class="form-check-input" id="agree" checked>
                             <label class="form-check-label" for="agree">Я даю согласие на обработку персональных данных</label>
                         </div>
                         <div class="button__wrapper">
-                            <button type="submit" class="btn btn-primary">отправить заявку</button>
+                            <button type="button" class="btn btn-primary">отправить заявку</button>
                         </div>
                     </form>
                 </div>
@@ -38,3 +38,17 @@
         </div>
     </div>
 </div>
+<script>
+    $("#popup_form button").on("click", function(){
+        $.ajax({
+            url: '/popup_form.php',
+            method: 'post',
+            dataType: 'html',
+            data: $("#popup_form").serialize(),
+            success: function(data){
+                console.log(data);
+                alert(data);
+            }
+        });
+    });
+</script>
