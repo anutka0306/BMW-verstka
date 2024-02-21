6ee8fc90fc4fbe391ef7a67416b829e715fda013
Андрей Ткаченко <tkachenko.dn@inbox.ru>
2024-02-21 10:24:03 +0300

    Добавлен видео виджет

---
+++ elements/video-widget.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="/css/video-widget.css">
+<script src="/js/video-widget.js" defer></script>
+<div class="video-widget" data-state="default">
+    <div class="video-widget__container">
+        <video id="video-widget__video" loop="" autoplay="" playsinline="" preload="auto" muted="muted"
+               controlslist="nodownload" disablepict
+               ureinpicture="" class="video-widget__video" src="/videos/pik_mini_bmw.mp4?dl=0">
+            <source src="/videos/pik_mini_bmw.mp4" type="video/mp4">
+        </video>
+        <div class="video-widget__close"></div>
+    </div>
+</div>
